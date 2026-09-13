27ad79792da64e3ac420ca101e0a53c84e14a491
spasso3d <spasso3d@gmail.com>
2026-09-13 19:01:59 -0300

    Com whatsapp

--- js/config.js
+++ js/config.js
@@ -6,6 +6,6 @@ const SUPABASE_KEY = "sb_publishable_3A86DGOcKa7LxaXqDKV0PA_VrEOVUzZ";
 
 // Número de WhatsApp para receber pedidos (formato: código do país + DDD + número, só números)
 // Ex: 55 11 99999-9999 -> "5511999999999"
-const WHATSAPP_NUMBER = "5511999999999"; // <-- TROQUE pelo número real
+const WHATSAPP_NUMBER = "5511972181804";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
